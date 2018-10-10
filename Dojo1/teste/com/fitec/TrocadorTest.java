ac41eb1406a42893bac312f969eb0fc579e3e97e
Walter Coelho <wcoelho@fitec.org.br>
2018-10-10 20:42:16 -0300

    Adicionada classe de teste unitário

---
+++ Dojo1/teste/com/fitec/TrocadorTest.java
@@ -0,0 +1,28 @@
+package com.fitec;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class TrocadorTest
+{
+
+    @Test
+    public void quandoPagtoMaiorQueDevido_retornaSucesso()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void quandoPagtoMenorQueDevido_retornaErro()
+    {
+        fail("Valor pago menor do que devido");
+    }
+
+    @Test
+    public void quandoPagto100Devido90_retorna1Cedula10()
+    {
+        fail("Not yet implemented");
+    }
+
+}
